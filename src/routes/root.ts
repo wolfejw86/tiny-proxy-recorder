3ddcc73cd542a20ba4ecc1c33e5825984223a3e3
Jay Wolfe <wolfejw86@gmail.com>
2022-05-17 22:29:01 -0400

    simplified response

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,10 +1,19 @@
 import { FastifyPluginAsync } from "fastify";
-import * as through2 from "through2";
-import * as pump from "pump";
 
 const cache = new Map<string, any>();
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  fastify.get("/webapp", async (request, reply) => {
+    reply.type("text/html");
+    return `<!DOCTYPE html>
+    <body>
+      <h1>Hello World!</h1>
+      <a href="/webapp/hello">Hello</a>
+    </body>
+    </html>
+    `;
+  });
+
   fastify.get("/*", {}, (request, reply) => {
     console.log(request.url);
     let actualResponse = "";
@@ -13,19 +22,24 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
       reply.send(cache.get(request.url));
     } else {
       reply.from(request.url, {
+        onError(reply, error) {
+          reply.send(error.error.message);
+        },
         onResponse: (request, reply, res) => {
-          pump(
-            res,
-            through2((chunk: any, enc: any, cb: any) => {
-              actualResponse += chunk.toString();
-              cb(null, chunk);
-            }),
-            reply.raw,
-            function (err) {
-              console.log(err);
+          res.on("data", (chunk) => {
+            console.log(chunk.toString());
+            actualResponse += chunk.toString();
+          });
+          res.on("end", () => {
+            try {
               cache.set(request.url, JSON.parse(actualResponse));
+              reply.send(actualResponse);
+            } catch (error: any) {
+              reply
+                .code(500)
+                .send(JSON.stringify({ ok: false, error: error.message }));
             }
-          );
+          });
         },
       });
     }
